f48dd4ea5d0049929f8b8077c55b40e95b40cffa
Marco Dali <marco@durazno.io>
2023-10-19 04:22:52 -0600

    scenes work sometimes

--- catecismo.js
+++ catecismo.js
@@ -1,29 +1,106 @@
 import 'dotenv/config'
 import WhatsAppBot from '@green-api/whatsapp-bot'
+import {
+    MENU_MESSAGE,
+    CATECISMO_MESSAGE,
+    FE_MESSAGE,
+    MISTERIO_MESSAGE,
+    VIDA_MESSAGE,
+    ORACION_MESSAGE,
+    EVANGELIO_MESSAGE,
+} from './messages.js'
 
-const initialMsg = `Buenos días, ¿Qué te gustaría consultar hoy?
-Escribe el número que corresponda a la opción.
-1. Catecismo 📖
-2. Reflexiones y temas de la vida diaria ✝️'
-3. Lecturas, evangelio y homilía del día 🕊️'
-4. El Rosario y oraciones 🙏'
-5. Santoral 😇'
-6. Música católica 🎵'
-`
+const session = WhatsAppBot.session
+const Stage = WhatsAppBot.Stage
+const Scene = WhatsAppBot.BaseScene
 
 const bot = new WhatsAppBot({
     idInstance: process.env.ID_INSTANCE,
     apiTokenInstance: process.env.API_TOKEN_INSTANCE
 })
 
-bot.on('message', async (ctx) => {
-    try {
-        ctx.reply(initialMsg)
-    } catch (error) {
-        console.error(error)
-        ctx.reply("No te entiendo, intenta de nuevo")
-    }
+// Main Menu scene
+const menuScene = new Scene('menu')
+menuScene.enter((ctx) => ctx.reply(MENU_MESSAGE))
+menuScene.leave((ctx) => ctx.reply('Dejando el menú principal, entrando a...'))
+menuScene.hears(['1'], async (ctx) => {
+    await ctx.scene.leave('menu')
+    ctx.scene.enter('catecismo')
+})
+menuScene.hears(['3'], async (ctx) => {
+    await ctx.scene.leave('menu')
+    ctx.scene.enter('evangelio')
 })
 
+// Catecismo scene
+const catecismoScene = new Scene('catecismo')
+catecismoScene.enter((ctx) => ctx.reply(CATECISMO_MESSAGE))
+catecismoScene.leave((ctx) => ctx.reply('Dejando Catecismo 📖, entrando a...'))
+catecismoScene.hears('1', async (ctx) => {
+    await ctx.scene.leave('catecismo')
+    ctx.scene.enter('fe')
+})
+catecismoScene.hears('2', async (ctx) => {
+    await ctx.scene.leave('catecismo')
+    ctx.scene.enter('misterio')
+})
+catecismoScene.hears('3', async (ctx) => {
+    await ctx.scene.leave('catecismo')
+    ctx.scene.enter('vida')
+})
+catecismoScene.hears('4', async (ctx) => {
+    await ctx.scene.leave('catecismo')
+    ctx.scene.enter('oracion')
+})
+catecismoScene.hears('5', async (ctx) => {
+    await ctx.scene.leave('catecismo')
+    ctx.scene.enter('menu')
+})
+
+// Fe scene
+const feScene = new Scene('fe')
+feScene.enter((ctx) => ctx.reply(FE_MESSAGE))
+feScene.leave((ctx) => ctx.reply('Saliendo de Fe 🏈'))
+feScene.hears('*', (ctx) => leave('fe'))
+
+// Misterio scene
+const misterioScene = new Scene('misterio')
+misterioScene.enter((ctx) => ctx.reply(MISTERIO_MESSAGE))
+misterioScene.leave((ctx) => ctx.reply('Saliendo de Misterio 🏓'))
+misterioScene.hears('*', async (ctx) => {
+    await ctx.scene.leave('misterio')
+    ctx.scene.enter('catecismo')
+})
+
+// Vida scene
+const vidaScene = new Scene('vida')
+vidaScene.enter((ctx) => ctx.reply(VIDA_MESSAGE))
+vidaScene.leave((ctx) => ctx.reply('Saliendo de Vida 🥏'))
+misterioScene.hears('*', async (ctx) => {
+    await ctx.scene.leave('vida')
+    ctx.scene.enter('catecismo')
+})
+
+// Oración scene
+const oracionScene = new Scene('oracion')
+oracionScene.enter((ctx) => ctx.reply(ORACION_MESSAGE))
+oracionScene.leave((ctx) => ctx.reply('Saliendo de Oracion 🛼'))
+misterioScene.hears('*', async (ctx) => {
+    await ctx.scene.leave('oracion')
+    ctx.scene.enter('catecismo')
+})
+
+// Evangelio scene
+const evangelioScene = new Scene('evangelio')
+evangelioScene.enter((ctx) => ctx.reply(EVANGELIO_MESSAGE))
+evangelioScene.leave((ctx) => ctx.reply('Saliendo de Evangelio 🕊️'))
+misterioScene.hears('*', async (ctx) => {
+    await ctx.scene.leave('evangelio')
+    ctx.scene.enter('catecismo')
+})
+
+const stage = new Stage([menuScene, catecismoScene, feScene, misterioScene, vidaScene, oracionScene, evangelioScene])
+bot.use(session())
+bot.use(stage.middleware())
+bot.on('message', (ctx) => ctx.scene.enter('menu'))
 bot.launch()
-console.log("bot started")
---
+++ messages.js
@@ -0,0 +1,45 @@
+export const MENU_MESSAGE = `*Menú Principal* 💎
+Buenos días, ¿Qué te gustaría consultar hoy?
+Escribe el número que corresponda a la opción.\n
+1 Catecismo 📖
+2 Temas de la vida diaria ✝️
+3 Evangelio y homilía del día 🕊️
+4 El Rosario y oraciones 🙏
+5 Santoral 😇
+6 Música católica 🎵
+`
+
+export const CATECISMO_MESSAGE = `*Catecismo* 📖
+El _Compendio_, presentado por el papa Benedicto XVI a la Iglesia Universal, \
+es una síntesis en formato de preguntas y respuestas del _Catecismo de la Iglesia Católica_. \
+Contiene, de modo conciso, todos los elementos esenciales y fundamentales de la fe de la Iglesia, \
+de manera tal que constituye, como deseaba el Papa Juan Pablo II, una especie de _manual_, \
+a través del cual las personas, creyentes o no, pueden abarcar con una sola mirada de conjunto \
+el panorama completo de la fe católica.\n
+¿Que parte del _catecismo_ te gustaría aprender? *Escribe* el número que corresponda a la opción.\n
+1 Primera parte - La profesión de la fe
+2 Segunda parte - La celebración del misterio cristiano
+3 Tercera parte - La vida en cristo
+4 Cuarta parte - La oración cristiana
+5 Regresar al menú principal
+`
+
+export const FE_MESSAGE = `Fe 🏈
+No options available here
+`
+
+export const MISTERIO_MESSAGE = `Misterio 🏓
+No options available here
+`
+
+export const VIDA_MESSAGE = `Vida 🥏
+No options available here
+`
+
+export const ORACION_MESSAGE = `Oración 🛼
+No options available here
+`
+
+export const EVANGELIO_MESSAGE = `Evangelio 🕊️
+No options available here
+`
